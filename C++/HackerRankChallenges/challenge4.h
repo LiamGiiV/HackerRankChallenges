40197d97264e4adb46e641e0b36a936a721e8def
LiamGiiV <43221209+LiamGiiV@users.noreply.github.com>
2022-08-11 14:26:36 -0400

    Challenge 4 passing

--- C++/HackerRankChallenges/challenge4.h
+++ C++/HackerRankChallenges/challenge4.h
@@ -1,5 +1,60 @@
 #pragma once
 
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <cstdio>
 using namespace std;
+
+string integerToEnglish(int input);
+
+void challenge4()
+{
+	int lowerBoundry = 0;
+	int upperBoundry = 0;
+
+	int result = scanf("%d\n%d", &lowerBoundry, &upperBoundry);
+
+	for (int index = lowerBoundry; index < upperBoundry; index++)
+	{
+		if (index <= 9 && index > 0)
+		{
+			printf(integerToEnglish(index).c_str());
+		}
+		else
+		{
+			if (index % 2 == 0)
+			{
+				printf("even\n");
+			}
+			else
+			{
+				printf("odd\n");
+			}
+		}
+	}
+}
+
+string integerToEnglish(int input)
+{
+	switch (input)
+	{
+	case 1:
+		return "one\n";
+	case 2:
+		return "two\n";
+	case 3:
+		return "three\n";
+	case 4:
+		return "four\n";
+	case 5:
+		return "five\n";
+	case 6: 
+		return "six\n";
+	case 7:
+		return "seven\n";
+	case 8:
+		return "eight\n";
+	case 9:
+		return "nine\n";
+	}
+	return "";
+}
