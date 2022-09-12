77b3fbf75d49e35ce55d5d8ba7e5d447400005a2
LiamGiiV <43221209+LiamGiiV@users.noreply.github.com>
2022-09-12 14:58:00 -0400

    Challenge 4 done!

--- HackerRankChallenges-Python/HackerRankChallenges_Python.py
+++ HackerRankChallenges-Python/HackerRankChallenges_Python.py
@@ -1,7 +1,9 @@
 import challenge1
 import challenge2
 import challenge3
+import challenge4
 
 challenge1.Challenge_1()
 challenge2.Challenge_2()
 challenge3.Challenge_3()
+challenge4.Challenge_4()
--- HackerRankChallenges-Python/challenge3.py
+++ HackerRankChallenges-Python/challenge3.py
@@ -1,2 +1,6 @@
 def Challenge_3():
-
+    a = int(input())
+    b = int(input())
+    print(a + b)
+    print(a - b)
+    print(a * b)
