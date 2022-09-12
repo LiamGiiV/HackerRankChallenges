43fa6d8ba48e704021d3fe908b48cc9ee7080701
LiamGiiV <43221209+LiamGiiV@users.noreply.github.com>
2022-09-12 15:42:38 -0400

    Challenge 7 done

--- HackerRankChallenges-Python/HackerRankChallenges_Python.py
+++ HackerRankChallenges-Python/HackerRankChallenges_Python.py
@@ -2,8 +2,14 @@ import challenge1
 import challenge2
 import challenge3
 import challenge4
+import challenge5
+import challenge6
+import challenge7
 
 challenge1.Challenge_1()
 challenge2.Challenge_2()
 challenge3.Challenge_3()
 challenge4.Challenge_4()
+challenge5.Challenge_5()
+challenge6.Challenge_6()
+challenge7.Challenge_7()
---
+++ HackerRankChallenges-Python/challenge7.py
@@ -0,0 +1,6 @@
+def Challenge_7():
+    n = int(input())
+    print(123, end='')
+    for element in range(n):
+        if element > 3 and element <= 150:
+            print(element, end='')
