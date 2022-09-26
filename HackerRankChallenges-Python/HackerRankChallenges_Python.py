f79f23cd53132d667dec72c97b10563802fde47b
LiamGiiV <43221209+LiamGiiV@users.noreply.github.com>
2022-09-26 09:56:46 -0400

    Challenge 9 done

--- HackerRankChallenges-Python/HackerRankChallenges_Python.py
+++ HackerRankChallenges-Python/HackerRankChallenges_Python.py
@@ -5,6 +5,8 @@ import challenge4
 import challenge5
 import challenge6
 import challenge7
+import challenge8
+import challenge9
 
 challenge1.Challenge_1()
 challenge2.Challenge_2()
@@ -13,3 +15,5 @@ challenge4.Challenge_4()
 challenge5.Challenge_5()
 challenge6.Challenge_6()
 challenge7.Challenge_7()
+challenge8.Challenge_8()
+challenge9.Challenge_9()
---
+++ HackerRankChallenges-Python/challenge9.py
@@ -0,0 +1,2 @@
+def Challenge_9():
+
